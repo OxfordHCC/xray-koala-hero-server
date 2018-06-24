fbd4b6d3f980ed39652db8cfb3be3bc3dff20c28
adam slack <adamslack@outlook.com>
2018-06-24 17:57:32 +0100

    just added a comment

--- koala/controllers/koala.controller.ts
+++ koala/controllers/koala.controller.ts
@@ -15,6 +15,8 @@ router.get('/', (req : Request, res: Response ) : void => {
             res.status(401).send({error:'invalid token'});
             return;
         }
+
+        // Do whaetever the endpoint do.
         res.send({"authenticated":true});
     });
 });
@@ -27,6 +29,8 @@ router.post('/something', (req : Request, res: Response ) : void => {
             res.status(401).send({error:'invalid token'});
             return;
         }
+
+        // Do whaetever the endpoint do.
         res.send({"something":"something"});
     });
 
