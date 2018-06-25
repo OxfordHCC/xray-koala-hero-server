fe79cdfb926b9239cab9dc359953f47b67f68e5c
Adam Slack <adamslack@outlook.com>
2018-06-25 18:03:58 +0100

    Revert "Study data endpoints"

--- koala/controllers/koala.controller.ts
+++ koala/controllers/koala.controller.ts
@@ -1,11 +1,8 @@
 import { Router, Request, Response, json } from 'express';
-import { AuthDetails, LogInteractionRequest, LogPhoneInformationRequest } from '../../util/types';
+import { AuthDetails } from '../../util/types';
 import * as jwt from 'jsonwebtoken';
-
 const config = require('../../config/config');
 
-import { DB } from '../../db/db';
-const db = new DB(config.roles.koala);
 
 const router : Router = Router();
 router.use(json());
@@ -24,49 +21,23 @@ router.get('/', (req : Request, res: Response ) : void => {
     });
 });
 
-router.post('/interaction', (req : Request, res: Response ) => {
-    let logInteractionRequest : LogInteractionRequest = req.body;
-    authenticate(logInteractionRequest.auth_details, async (err : jwt.JsonWebTokenError, decoded) => {
+router.post('/something', (req : Request, res: Response ) : void => {
+    let authDetails : AuthDetails = req.body;
+    authenticate(authDetails, (err : jwt.JsonWebTokenError, decoded) => {
         if(err) {
             console.log(err);
             res.status(401).send({error:'invalid token'});
             return;
         }
 
-        try{
-            await db.insertInteractionLog(logInteractionRequest.interaction);
-            res.status(200).send({Success:"Interaction was successfully logged."});
-        }
-        catch(err) {
-            res.status(400).send({Error:err});
-        }
-            // Do whaetever the endpoint do.
+        // Do whaetever the endpoint do.
+        res.send({"something":"something"});
     });
-});
 
-router.post('/phone_info', (req : Request, res: Response ) => {
-    let logPhoneInformationRequest : LogPhoneInformationRequest = req.body;
-    console.log(logPhoneInformationRequest);
-    authenticate(logPhoneInformationRequest.auth_details, async (err : jwt.JsonWebTokenError, decoded) => {
-        if(err) {
-            console.log(err);
-            res.status(401).send({error:'invalid token'});
-            return;
-        }
-
-        try{
-            await db.insertPhoneInfo(logPhoneInformationRequest.phone_info);
-            res.status(200).send({Success:"Interaction was successfully logged."});
-        }
-        catch(err) {
-            res.status(400).send({Error:err});
-        }
-            // Do whaetever the endpoint do.
-    });
 });
 
 function authenticate(authDetails : AuthDetails, callback : jwt.VerifyCallback) {
-    return jwt.verify(authDetails.token, config.auth.secret, { jwtid:authDetails.study_id }, callback);
+    return jwt.verify(authDetails.token, config.auth.secret, { jwtid:authDetails.email }, callback);
 }
 
 export const KoalaController : Router = router;
